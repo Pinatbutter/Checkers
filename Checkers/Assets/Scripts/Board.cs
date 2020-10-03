32ad9a7547cd0981b4ddd81a68b4658f2b431d31
Pinatbutter <dpina@harding.edu>
2020-10-02 21:03:34 -0500

    Made Pieces Move

--- Checkers/Assets/Scripts/Board.cs
+++ Checkers/Assets/Scripts/Board.cs
@@ -1,28 +1,89 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+
     public Pieces[,] piece = new Pieces[8, 8];
+    public Vector3 offSet = new Vector3(-4.0f, 0, -4.0f);
+    public Vector3 otherOffSet = new Vector3(0.5f, 0, 0.5f);
+
     public GameObject whitePiecePrefab;
     public GameObject blackPiecePrefab;
-    private Vector3 offSet = new Vector3(-4.0f, 0, -4.0f);
-    private Vector3 otherOffSet = new Vector3(0.5f, 0, 0.5f);
 
+    private Pieces selectedPiece;
+    private Vector2 mouseOver;
+    private Vector2 startDrag;
+    private Vector3 endDrag;
 
     private void Start()
     {
         GeneratePieces();
     }
 
+    private void Update()
+    {
+        UpdateMouseOver();
+        Debug.Log(mouseOver);
+        {
+            int x = (int)mouseOver.x;
+            int y = (int)mouseOver.y;
+            if (Input.GetMouseButtonDown(0))
+                SelectPiece(x, y);
+            if (Input.GetMouseButtonUp(0))
+            {
+                TryMove((int)startDrag.x, (int)startDrag.y, x, y);
+            }
+        }
+    }
+    private void UpdateMouseOver()
+    {
+        if (!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            mouseOver.x = (int)(hit.point.x - offSet.x);
+            mouseOver.y = (int)(hit.point.z - offSet.z);
+        }
+        else
+        {
+            mouseOver.x = -1;
+            mouseOver.y = -1;
+        }
+    }
+    private void SelectPiece(int x, int y)
+    {
+        if (x < 0 || x >= piece.Length || y < 0 || y >= piece.Length)
+            return;
+
+        Pieces p = piece[x, y];
+        if (p != null)
+        {
+            selectedPiece = p;
+            startDrag = mouseOver;
+            Debug.Log(selectedPiece.name);
+        }
+    }
+    private void TryMove(int x1, int y1, int x2, int y2)
+    {
+        startDrag = new Vector2(x1, y1); 
+        endDrag = new Vector2(x2, y2);
+        selectedPiece = piece[x1, y1]; 
+
+        PlacePiece(selectedPiece, x2, y2);
+    }
     private void GeneratePieces()
     {
         //white pieces
         for (int y = 0; y < 3; y++)
         {
             bool oddRow = (y % 2 == 0);
-            for (int x =0; x<8; x += 2)
+            for (int x = 0; x < 8; x += 2)
             {
                 if (oddRow)
                     CreatePiece(x, y);
@@ -62,6 +123,7 @@ public class Board : MonoBehaviour
     private void PlacePiece(Pieces p, int x, int y)
     {
         p.transform.position = (Vector3.right * x) + (Vector3.forward * y) + offSet + otherOffSet;
+
     }
 
 
